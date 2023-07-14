5895e519bed38caa27d1c5726ea4f6c910d7b059
Carson <77071807+csklare101@users.noreply.github.com>
2023-07-14 12:30:56 -0500

    Create New Years Chaos.java
    7/14/23

---
+++ New Years Chaos.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'minimumBribes' function below.
+     *
+     * The function accepts INTEGER_ARRAY q as parameter.
+     */
+
+    public static void minimumBribes(List<Integer> q) {
+    // Write your code here
+    List<Integer> initial = new ArrayList<>(q);
+    Collections.sort(initial);
+    if(initial.equals(q)){
+        System.out.println(0);
+        return;    
+    }
+    
+    int size = q.size();
+    int bribes = 0;
+    boolean chaotic = false;
+    int p = 0;
+    
+    for(int i = 0; i < size; i++){
+        p = q.get(i);
+        if(p - (i+1) > 2){
+            System.out.println("Too chaotic");
+            chaotic = true;
+            break;
+        }
+        
+        for(int j = Math.max(0, p-2); j < i; j++){
+            if(q.get(j) > p){
+                bribes++;
+            }
+        }
+    }
+    
+    if(!chaotic){
+        System.out.println(bribes);
+    }
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, t).forEach(tItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> q = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                Result.minimumBribes(q);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+    }
+}
